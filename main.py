fc08cb86b0aee649b739255247823073d001b170
v2 <Docilomaksim@gmail.com>
2026-03-13 19:49:24 +0200

    v3

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-from turtle import *
-color('red', 'yellow')
-begin_fill()
-while True:
-    forward(200)
-    left(170)
-    if abs(pos()) < 1:
-        break
-end_fill()
-done()
+name = input("Введіть ваше ім'я: ")
+age = input("Введіть ваш вік: ")
+
+print("Привіт", name + ", тобі", age + "!")
